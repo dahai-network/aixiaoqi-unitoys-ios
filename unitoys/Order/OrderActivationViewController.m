cf8818ce7e1397888eda97dae43c3f4f53cf14d9
dongheiheihei <630893613@qq.com>
2017-06-08 18:22:24 +0800

    完善刷新显示弹框问题
    完善刷新显示弹框问题

--- unitoys/Order/OrderActivationViewController.m
+++ unitoys/Order/OrderActivationViewController.m
@@ -169,7 +169,9 @@
         {
             [self startAnimation];
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [self showNotBoundAlertWindow];
+                if (![self.lblDeviceStatus.text isEqualToString:@"已连接"]) {
+                    [self showNotBoundAlertWindow];
+                }
             });
         }
             break;
